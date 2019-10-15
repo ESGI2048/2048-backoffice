5053b36bd6dfdea86c4e5252efc1f9e551a6432b
SystemGlitch <jeremy.la@outlook.fr>
2019-10-15 18:48:40 +0200

    Install buefy and set style

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <button class="button is-primary">Primary</button>
+    <h1 class="title">This is an about page</h1>
+    <b-button type="is-dark">Primary</b-button>
   </div>
 </template>
